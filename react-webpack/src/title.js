0ce4e26aab9e05b3e8e37e111b9268240774d0cf
lucas2502 <lucas2502@live.com>
2018-11-23 16:47:05 +0000

    Passando outros tipos de Props

--- react-webpack/src/title.js
+++ react-webpack/src/title.js
@@ -3,8 +3,16 @@
 import React from 'react'
 
 const Title = React.createClass({
+    getDefaultProps: function(){
+        return {
+            name: 'Desconhecido',
+            lastname: 'Sem sobrenome'
+        }
+    },
     render: function(){
-        return <h1>Ola {this.props.name}!</h1>
+        return (
+            <h1>Ola {this.props.name + ' ' + this.props.lastname}!</h1>
+        )
     }
 })
 
