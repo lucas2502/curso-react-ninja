f8c7012f9e2b92a4564a35e8bad016acc750eb37
lucas2502 <lucas2502@live.com>
2019-01-17 14:40:09 +0000

    M1#A1 Children

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,15 +1,13 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Square from './square' 
+import Button from './button' 
 
 class App extends Component{
     render(){
         return (
-            <div className="container" onClick={function (e){
-                alert('clicou')
-            }}>
-                <Square/>
+            <div className="container">
+                <Button>Texto</Button>
             </div>
         )
     }
---
+++ react-webpack/src/button.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import React from 'react'
+
+const Button = ({children}) => (
+    <button>{children}</button>
+)
+
+
+export default Button
