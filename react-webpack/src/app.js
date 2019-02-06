c3dcd994ae257362f4c6ea5ce27d23dabffbfc5e
lucas2502 <lucas2502@live.com>
2019-02-06 15:49:23 -0200

    Stateful vs stateless

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -2,12 +2,25 @@
 
 import React, { Component } from 'react'
 import Button from './button' 
+import Square from  './square'
 
 class App extends Component{
+    constructor () {
+        super()
+        this.state = {
+            color: 'green'
+        }
+    }
     render(){
         return (
-            <div className="container">
-                <Button>Texto</Button>
+            <div>
+                <Square color={this.state.color} />
+
+                {['red', 'green', 'blue'].map((color) => (
+                    <Button key={color} handleClick={() => this.setState({ color })}>
+                    {color}
+                    </Button>
+                ))}
             </div>
         )
     }
--- react-webpack/src/button.js
+++ react-webpack/src/button.js
@@ -2,8 +2,10 @@
 
 import React from 'react'
 
-const Button = ({children}) => (
-    <button>{children}</button>
+const Button = ({children, handleClick}) => (
+    <button classname='main-button' onCLick={handleClick}>
+    {children}
+    </button>
 )
 
 
