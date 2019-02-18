8248e38386c05985afe415317e7f794b22aee76b
lucas2502 <lucas2502@live.com>
2019-02-18 12:04:17 -0300

    Fazendo resquest dos dados do Usuario

--- gitHubApp/src/components/search.js
+++ gitHubApp/src/components/search.js
@@ -1,12 +1,24 @@
 'use strict'
 
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = () => (
     <div className='search'>
         <input 
             type='search' 
-            placeholder='Digite o nome de usuário no GitHub' 
+            placeholder='Digite o nome de usuário no GitHub'
+            onKeyUp={(e) => {
+                const value = e.target.value
+                const keyCode = e.which || e.keyCode
+                const ENTER = 13
+                if( keyCode === ENTER){
+                    ajax().get(`https://api.github.com/users/${value}`)
+                    .then((result) =>{
+                        console.log(result)
+                    })
+                }
+            }}
         />
     </div>
 )
