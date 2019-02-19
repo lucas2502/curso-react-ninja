f1797220cdddbab1116122259eec86f9e9d1fc6a
lucas2502 <lucas2502@live.com>
2019-02-18 21:11:40 -0300

    Buscar repositorio do ususario pesquisado

--- gitHubApp/src/app.js
+++ gitHubApp/src/app.js
@@ -13,13 +13,18 @@ class App extends Component {
             starred: []
         }
     }
+    getGitHubApiUrl (username, type) {
+        const internalUser = username ? `/${username}` : ''
+        const internalType = type ? `/${type}` : '' 
+        return `https://api.github.com/users/${internalUser}${internalType}`
+    }
     handleSearch (e) {
         const value = e.target.value
         const keyCode = e.which || e.keyCode
         const ENTER = 13
 
         if( keyCode === ENTER){
-            ajax().get(`https://api.github.com/users/${value}`)
+            ajax().get(this.getGitHubApiUrl(value))
             .then((result) => {
             this.setState({
                 userinfo: {
@@ -29,7 +34,9 @@ class App extends Component {
                     repos: result.public_repos,
                     followers: result.followers,
                     following: result.following
-                }
+                },
+                repos: [],
+                sterred: []
             })
             })
         }
@@ -37,7 +44,8 @@ class App extends Component {
     
     getRepos (type) {
         return (e) => {
-            ajax().get(`https://api.github.com/users/lucas2502/${type}`)
+            const username = this.state.userinfo.login
+            ajax().get(this.getGitHubApiUrl(username, type))
             .then((result) => {
                     this.setState({
                         [type]: result.map((repo) => {
