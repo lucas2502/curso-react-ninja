166cb7c2490d61eab6afeac829f5e4897a5c9aba
lucas2502 <lucas2502@live.com>
2019-05-03 16:55:20 -0300

    Jest in pratice (MAP)

---
+++ jest/src/map/map.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const map = (arr, func) => {
+    let newArr = []
+    for ( let i = 0; i < arr.length; i++){
+        newArr.push(func(arr[i], i, arr))
+    }
+    return newArr
+}
+
+export default map
